cde4a4468a2b88f5f5d3faecf3832507ead3719d
1eedaegon <d8726243@gmail.com>
2023-07-17 19:31:39 +0900

    Fin practice

--- 03-make-regcred-dockerhub.sh
+++ 03-make-regcred-dockerhub.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=1eedaegon --docker-email=d8726243@gmail.com
+kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1 --docker-username=1eedaegon --docker-email=d8726243@gmail.com
